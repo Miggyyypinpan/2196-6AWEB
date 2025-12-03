4f92f6c8e12900919978afdfb10a19a84a8d5c47
miggyyypinpan <miggypin1218@gmail.com>
2025-12-04 00:52:14 +0800

    feat: Add 404 page not found component and integrate Bootstrap styles

--- Prelim Lab Routing Demo - Act 4 & 5/src/app/app.routes.ts
+++ Prelim Lab Routing Demo - Act 4 & 5/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
 
 export const routes: Routes = [
   {path:  '', component : Home},
   {path: 'data-binding', component : DataBinding},
-  {path: 'directives', component : Directives}
+  {path: 'directives', component : Directives},
+  {path: '**', component : Pagenotfound}
 ];
